819e7703799802c0073c1ceb2c974fecde49503e
Nikita Serov <nikita-serov2003@yandex.ru>
2024-12-15 17:32:29 +0500

    updated experiments

--- src/pages/ExperimentContentPage.tsx
+++ src/pages/ExperimentContentPage.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import IntegrationExperimentPage from './IntegrationExperimentPage';
 
 const ExperimentContentPage: React.FC = () => {
-  const { experimentId } = useParams<{ experimentId: string }>();
+  const { experimentId } = useParams<{ experimentId?: string }>();
+
+  const renderExperimentContent = (id: string) => {
+    switch (id) {
+      case '1':
+        return <IntegrationExperimentPage />;
+      default:
+        return <div>Эксперимент не найден</div>;
+    }
+  };
+
+  if (!experimentId) {
+    return <div>Эксперимент не выбран</div>;
+  }
 
   return (
     <div style={{ height: '100vh', width: '100%' }}>
-      <iframe
-        src={`https://mathexperiment${experimentId}.streamlit.app?embed=true`}
-        style={{ width: '100%', height: '100%', border: 'none' }}
-        title="Streamlit Experiment"
-      />
+      {renderExperimentContent(experimentId)}
     </div>
   );
 };
